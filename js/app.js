3fea4943a957af49e342aef874c8ab566ced5000
ronymia <official.mdronymia@gmail.com>
2022-03-03 09:16:40 +0600

    temp added

--- js/app.js
+++ js/app.js
@@ -3,12 +3,18 @@ const API_KEY = `5ef6423e5a17bc7ca85f13c6a1f6bac6`
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
     // console.log(city);
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&&units=metric`
     // console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemperature(data));
-}
+};
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+};
+
 const displayTemperature = temperature => {
     console.log(temperature);
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
 };
